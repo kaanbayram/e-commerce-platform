b916f2bf03d2076d03fb88da08d77eaa125fff4e
kaanbayram <kaan.9898@hotmail.com>
2022-08-07 23:32:50 +0300

    [dev] setting dotenv module and inspecting 12 factor app 3. config factor.

--- backend/server.ts
+++ backend/server.ts
@@ -2,16 +2,21 @@ import express from 'express';
 // const express = require('express');
 const products = require('./data/products')
 const cors = require("cors");
+const dotenv = require('dotenv');
+
+dotenv.config()
 
 const app = express();
 
 
-app.listen(5000, () => { console.log("Working on 5000") });
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => { console.log(`Server running in ${process.env.NODE_ENV} on port ${process.env.PORT}`) });
 
 // var corsOptions = {
 //     origin: "http://http://192.168.1.7:5000"
 //   };
-  
+
 //   app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
